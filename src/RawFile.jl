8389736d5a40802c68cb6e359ae9ae703613547e
Bill Gross <bill.gross@me.com>
2017-11-28 16:48:43 +0000

    first commit

--- src/RawFile.jl
+++ src/RawFile.jl
@@ -1,5 +1,47 @@
 module RawFile
 
-# package code goes here
+export saveraw,readraw
 
-end # module
+token = "RAWF001"
+endtoken = "FIN"
+
+rawtypekey = Type[
+    Float64,
+    Float32,
+    Float16,
+    Int64  ,
+    Int32  ,
+    Int16  ,
+    Int8   ,
+    UInt8
+]
+
+
+function saveraw{T,V}(a::AbstractArray{T,V},fname::String)
+    open(fname,"w") do f
+        write(f,token)
+        write(f,Int8(findfirst(rawtypekey,typeof(a[1]))))
+        write(f,Int8(V))
+        write(f,collect(size(a)))
+        write(f,a)
+        write(f,endtoken)
+    end
+end
+
+function readraw(fname::String)
+    open(fname) do f
+        tok = String(read(f,length(token)))
+        tok != token && error("Invalid token in raw file")
+        typenum = read(f,Int8)
+        typet = rawtypekey[typenum]
+        nd = read(f,Int8)
+        siz = read(f,Int64,nd)
+        d = read(f,typet,Tuple(siz))
+        endtok = String(read(f,length(endtoken)))
+        endtok != endtoken && error("Invalid end of file")
+        return d
+    end
+end
+
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using RawFile
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+fname = "testfile.raw"
+for t in RawFile.rawtypekey
+    for d in [rand(t,100),rand(t,75,100),rand(t,10,30,60)]
+        saveraw(d,fname)
+        dd = readraw(fname)
+        @assert d == dd
+        isfile(fname) && rm(fname)
+    end
+end
