490f83311c8cc6d5b4de650e06d8c1c80969109a
Bill Gross <bill.gross@me.com>
2017-11-28 18:10:19 +0000

    Added batch reading

--- src/RawFile.jl
+++ src/RawFile.jl
@@ -2,7 +2,8 @@ module RawFile
 
 export saveraw,readraw
 
-token = "RAWF001"
+token = "RAWF"
+version = 1
 endtoken = "FIN"
 
 rawtypekey = Type[
@@ -16,13 +17,39 @@ rawtypekey = Type[
     UInt8
 ]
 
+type RawHeader
+    version::Int8
+    typenum::Int8
+    typet::Type
+    sizes::Array{Int64,1}
+end
 
-function saveraw{T,V}(a::AbstractArray{T,V},fname::String)
+import Base.write
+
+function write(f::IO,x::RawHeader)
+    write(f,token)
+    write(f,x.version)
+    write(f,x.typenum)
+    write(f,Int8(length(x.sizes)))
+    write(f,x.sizes)
+end
+
+function readheader(f::IO)
+    tok = String(read(f,length(token)))
+    tok != token && error("Invalid token in RawFile")
+    ver = read(f,Int8)
+    typenum = read(f,Int8)
+    typet = rawtypekey[typenum]
+    nd = read(f,Int8)
+    sizes = read(f,Int64,nd)
+    RawHeader(ver,typenum,typet,sizes)
+end
+
+function saveraw{T<:Real,V}(a::AbstractArray{T,V},fname::String)
+    typenum = Int8(findfirst(rawtypekey,typeof(a[1])))
+    header = RawHeader(version,typenum,typeof(a[1]),collect(size(a)))
     open(fname,"w") do f
-        write(f,token)
-        write(f,Int8(findfirst(rawtypekey,typeof(a[1]))))
-        write(f,Int8(V))
-        write(f,collect(size(a)))
+        write(f,header)
         write(f,a)
         write(f,endtoken)
     end
@@ -30,16 +57,30 @@ end
 
 function readraw(fname::String)
     open(fname) do f
-        tok = String(read(f,length(token)))
-        tok != token && error("Invalid token in raw file")
-        typenum = read(f,Int8)
-        typet = rawtypekey[typenum]
-        nd = read(f,Int8)
-        siz = read(f,Int64,nd)
-        d = read(f,typet,Tuple(siz))
+        h = readheader(f)
+        d = read(f,h.typet,Tuple(h.sizes))
         endtok = String(read(f,length(endtoken)))
-        endtok != endtoken && error("Invalid end of file")
+        endtok != endtoken && error("Invalid end of RawFile")
         return d
+    end
+end
+
+function readraw(func::Function,fname::String,batch::Int)
+    open(fname) do f
+        h = readheader(f)
+        batch_step = reduce(*,h.sizes[1:end-1])
+        total_length = batch_step*h.sizes[end]
+        batch_size = copy(h.sizes)
+        i = 0
+        while i<total_length
+            this_len = Int(min(batch,(total_length-i)/batch_step))
+            batch_size[end] = this_len
+            d = read(f,h.typet,Tuple(batch_size))
+            func(d)
+            i += batch_step*this_len
+        end
+        endtok = String(read(f,length(endtoken)))
+        endtok != endtoken && error("Invalid end of RawFile")
     end
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,22 @@ using Base.Test
 
 fname = "testfile.raw"
 for t in RawFile.rawtypekey
-    for d in [rand(t,100),rand(t,75,100),rand(t,10,30,60)]
+    for d in [rand(t,100),rand(t,75,100),rand(t,10,100)]
         saveraw(d,fname)
         dd = readraw(fname)
+        catfunc = ndims(d)==1 ? vcat : hcat
+        dd_batch = 0
+        readraw(fname,10) do d
+            if dd_batch==0
+                dd_batch = d
+            else
+                dd_batch = catfunc(dd_batch,d)
+            end
+        end
+        
         @assert d == dd
+        @assert d == dd_batch
+        
         isfile(fname) && rm(fname)
     end
 end
